4de312826f1d46caa53482baf480841d516240a8
Sibusiso <sikblessing4@gmail.com>
2025-07-29 04:51:26 +0200

    Configure HTTPS for Vite and ignore ssl folder

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,18 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios';
 
+// This is the main functional component for the React app
 function App() {
+  // useState is a React Hook that defines a state variable named 'backendMessage'
+  // It starts with an empty string and will be updated later with data from the backend
   const [backendMessage, setBackendMessage] = useState('');
 
   useEffect(() => {
+    // Send a GET request to the backend API endpoint at http://localhost:5000/test
       axios.get('http://localhost:5000/test')
       .then(Response => {
+        // If the request succeeds, set the 'backendMessage' state with the message returned from the backend
         setBackendMessage(Response.data.message);
       })
       .catch(error => {
+        // If there's an error (like backend not running), log it to the browser console
         console.error("Error fetching backend data:", error);
       });
-  }, []);
+  }, []);  // The empty array means this useEffect runs only once when the component first loads
 
   return (
     <>
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,18 @@
+// this code configures the React Frontend (using Vite) to run HTTPS locally using the same self-signed cert
+// the 'vite.config.js' is used to define how the dev server behaves  
+
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // this adds support to Vite 
+import fs from 'fs'; // built-in Node.js module to read the local cert and PK files from teh disk
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()], // adding React plugin to make sure JSX and other React features are properly handled 
+  // telling Vite to run its development server over HTTPS
+  server: {
+    https: {
+      key: fs.readFileSync('ssl/key.pem'), 
+      cert: fs.readFileSync('ssl/cert.pem'),
+    }
+  }
+});
